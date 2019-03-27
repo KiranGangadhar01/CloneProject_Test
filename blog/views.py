b6dfc5736e28337f67f9e855e32b660c4a01e700
KiranGangadhar01 <kiran.gangadhar.01@gmail.com>
2019-03-27 11:58:23 +0530

    Modified views

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
-
+from django.contrib.auth.mixins iport LoginRequiredMixin
+from django.views.generic import (TemplateView, ListView,
+                                    DetailView, CreateView,
+                                    UpdateView, DeleteView)
+from .models import Post,Comment
+from .forms import PostForm, CommentForm
+from django.utils import timezone
+from django.urls import reverse_lazy
 # Create your views here.
+
+class AboutView(TemplateView):
+    template_name = 'blog/about.html'
+
+class PostListView(ListView):
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+class PostDetailView(DetailView):
+    model = Post
+
+class CreatePostView(LoginRequiredMixin, CreateView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_detail.html'
+
+    form_class = PostForm
+
+    model = Post
